525c524733fad72baa9d5eefe5c4ed66e4b492db
mateuszkowalczykk <dev.mateuszkowalczyk@gmail.com>
2022-04-11 20:35:01 +0200

    add spring-actuator dependency, set app default timezone

--- src/main/java/pl/mateuszkowalczykk/rest/RestApplication.java
+++ src/main/java/pl/mateuszkowalczykk/rest/RestApplication.java
@@ -1,5 +1,6 @@
 package pl.mateuszkowalczykk.rest;
 
+import java.util.TimeZone;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class RestApplication {
 
   public static void main(String[] args) {
+    TimeZone.setDefault(TimeZone.getTimeZone("Poland"));
     SpringApplication.run(RestApplication.class, args);
   }
 
